7b4d630963ba21da9c8b4235e912975e2ef80d46
ashrafstakala <ashrafstakala@gmail.com>
2022-10-30 21:13:08 +0200

    project setup

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,8 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export const config: Config = {
   namespace: 'stellar-wallet',
@@ -8,9 +12,6 @@ export const config: Config = {
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements',
-    },
-    {
       type: 'docs-readme',
     },
     {
@@ -18,4 +19,22 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
+  globalStyle: 'src/global/style.scss',
+  commonjs: {
+    namedExports: {
+      'stellar-sdk': ['StrKey', 'xdr', 'Transaction', 'Keypair', 'Networks', 'Account', 'TransactionBuilder', 'BASE_FEE', 'Operation', 'Asset', 'Memo', 'MemoHash'],
+      '@stellar/wallet-sdk': ['KeyManager', 'KeyManagerPlugins', 'KeyType'],
+    },
+  },
+  plugins: [
+    nodePolyfills(),
+    sass(),
+    postcss({
+      plugins: [autoprefixer()],
+    }),
+  ],
+  nodeResolve: {
+    browser: true,
+    preferBuiltins: true,
+  },
 };
